3598f01ff7b22db311c62c087f5bd01f92ad4b58
Sentiurin Vladimir <sentyurin@gmail.com>
2016-07-06 07:54:06 +0300

    socket.io fix-0

--- socket/index.js
+++ socket/index.js
@@ -2,7 +2,6 @@
 module.exports = function(server) {
 
   var io = require('socket.io')(server);
-  io.set('origins', 'localhost:*');
 
   io.on('connection', function(socket) {
     console.log('user connected');
